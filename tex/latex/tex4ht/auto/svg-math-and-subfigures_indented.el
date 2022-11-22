cbeb071a6857a5126c4b520752287096b1da7a61
llorracc <git@llorracc.org>
2022-11-22 17:24:33 -0500

    Restore links rather than files

--- tex/latex/tex4ht/auto/svg-math-and-subfigures_indented.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "svg-math-and-subfigures_indented"
- (lambda ()
-   (LaTeX-add-environments
-    '("labeling" LaTeX-env-args ["argument"] 1)))
- :latex)
-
--- tex/latex/tex4ht/auto/svg-math-and-subfigures_tex4ht-and-table-of-contents-fix.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "svg-math-and-subfigures_tex4ht-and-table-of-contents-fix"
- (lambda ()
-   (TeX-add-symbols
-    "c"
-    "tocNesting"))
- :latex)
-
